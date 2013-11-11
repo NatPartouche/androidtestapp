6cd4df3c50c41ae973b7aa31c9a5b16db77a26de
NatPartouche <natanel.partouche@gmail.com>
2013-11-11 18:42:34 +0100

    plugin_installer.sh

---
+++ testandroid/plugin_installer.sh
@@ -0,0 +1,40 @@
+#Basic device information (Device API):
+phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git
+
+#Network Connection and Battery Events:
+phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-network-information.git
+phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-battery-status.git
+
+#Accelerometer, Compass, and Geolocation:
+
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device-motion.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device-orientation.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
+
+#Camera, Media playback and Capture:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-camera.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-media-capture.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-media.git
+
+#Access files on device or network (File API):
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-file.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-file-transfer.git
+
+#Notification via dialog box or vibration:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-dialogs.git
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-vibration.git
+
+#Contacts:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-contacts.git
+
+#Globalization:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git
+
+#Splashscreen:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-splashscreen.git
+
+#Debug console:
+ phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-console.git
+
+#list
+phonegap local plugin list
